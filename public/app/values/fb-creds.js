81068190bcc00b71a52d8366ed7437c56ded873c
Kayla Brewer <cooper.kaylam@gmail.com>
2017-06-19 20:54:19 -0500

    added hosted link to readme

---
+++ public/app/values/fb-creds.js
@@ -0,0 +1,10 @@
+'use strict';
+
+app.constant('FBCreds',
+    {
+        apiKey: "AIzaSyB7oJvS1GcNUjW_WhOnWGzTTIroiC2_Ji8",
+        authDomain: "tress-tracker.firebaseapp.com",
+        databaseURL: "https://tress-tracker.firebaseio.com",
+        storageBucket: "tress-tracker.appspot.com",
+        messagingSenderId: "392864950472",
+    });
